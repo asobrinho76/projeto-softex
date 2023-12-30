9c8b7004cd0191455607a1bcb9abd54c09804e68
Silvana Paiva <sil_paiva@hotmail.com>
2023-12-29 23:41:17 -0300

    Add node e ajustes

--- src/pages/midias/Cad_midias.jsx
+++ src/pages/midias/Cad_midias.jsx
@@ -1,43 +1,13 @@
-import React from 'react';
-import TextBox from '../../components/textbox/TextBox';
-import './CadMidias.css';
-import ButtonUpload from '../../components/Buttons/buttonUpload/ButtonUpload';
-import Label from '../../components/Texts/label/Label';
-import ButtonSalve from '../../components/Buttons/buttonSalve/ButtonSalve';
-import Title from '../../components/Texts/Title/Title';
-import SubTitle from '../../components/Texts/Subtitle/Subtitle';
-import CheckboxMidia from '../../components/CheckBox/CheckBoxMidia';
-import Subtitle from '../../components/Texts/Subtitle/Subtitle';
-import ResponsiveDatePickers from '../../components/Calendar/Calendar'
+import React from 'react'
+import './Cad_midias.css'
 
 
-
-
-
-
-
-
-
-
-export default function Cad_Midia() {
+const Cad_midias = () => {
   return (
-    <>
-    <header>
-    <Title text="Cadastro de Mídias"/>
-    </header>
-    <section>
-      <div>      
-    <TextBox label="Titulo"/>
-    <TextBox label="Descrição"/>
-    <SubTitle text="Tipo de Midia"/>
-    <CheckboxMidia/>
-    <ButtonUpload label="Anexar"/>
-    <Label text1="*Imagem até 50MB" text2="*Vídeo até 5 minutos"/>
-    <Subtitle text="Período de exibição"/>
-    <ResponsiveDatePickers/>
-    <ButtonSalve text="Salvar"/>
-    </div>
-    </section>
-    </>
+    <h1>Cadastro de mídias</h1>
   )
 }
+
+export default Cad_midias
+
+
