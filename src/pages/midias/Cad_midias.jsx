78c19a2e3aee6ce707e1421a1203c872e140c744
Carolaine Oliveira <122393936+CarolaineOliver@users.noreply.github.com>
2023-12-29 21:29:59 -0300

    Inclusão da tela de cadastro de Mídias

--- src/pages/midias/Cad_midias.jsx
+++ src/pages/midias/Cad_midias.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import './Cad_midias.css'
+import React from 'react';
+import TextBox from '../../components/textbox/TextBox';
+import './CadMidias.css';
+import ButtonUpload from '../../components/Buttons/buttonUpload/ButtonUpload';
+import Label from '../../components/Texts/label/Label';
+import ButtonSalve from '../../components/Buttons/buttonSalve/ButtonSalve';
+import Title from '../../components/Texts/Title/Title';
+import SubTitle from '../../components/Texts/Subtitle/Subtitle';
+import CheckboxMidia from '../../components/CheckBox/CheckBoxMidia';
+import Subtitle from '../../components/Texts/Subtitle/Subtitle';
+import ResponsiveDatePickers from '../../components/Calendar/Calendar'
 
 
-const Cad_midias = () => {
+
+
+
+
+
+
+
+
+export default function Cad_Midia() {
   return (
-   <h1>Cadastro de mídias</h1>
+    <>
+    <header>
+    <Title text="Cadastro de Mídias"/>
+    </header>
+    <section>
+      <div>      
+    <TextBox label="Titulo"/>
+    <TextBox label="Descrição"/>
+    <SubTitle text="Tipo de Midia"/>
+    <CheckboxMidia/>
+    <ButtonUpload label="Anexar"/>
+    <Label text1="*Imagem até 50MB" text2="*Vídeo até 5 minutos"/>
+    <Subtitle text="Período de exibição"/>
+    <ResponsiveDatePickers/>
+    <ButtonSalve text="Salvar"/>
+    </div>
+    </section>
+    </>
   )
 }
-
-export default Cad_midias
