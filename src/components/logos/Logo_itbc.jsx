c8730cd199aefcdb33169d7273483783b3ba786a
Amanda Suamy <anandasuamy@gmail.com>
2024-01-04 20:21:27 -0300

    att navbar

--- src/components/logos/Logo_itbc.jsx
+++ src/components/logos/Logo_itbc.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import itbc from '/src/imagens/logos/itbc.png'
+import itbc from '/src/imagens/logos/itbc.svg'
 import './Logo_itbc.css'
 
 const Logo_itbc = () => {
   return (
-    <div>
+    <div className='logo-itbc'>
       <img src={itbc} alt='logomarca itbc' />
     </div>
   )
